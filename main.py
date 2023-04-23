73262124c957bc277adb2824e82234278d33a231
jcabben <JCab2611@gmail.com>
2023-04-23 18:38:57 -0400

    Added gs data + preprocessing
    Formatted train data to account for new lines

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from preprocessing import preprocessor
+from nltk import HiddenMarkovModelTagger
+import numpy as np
 
 def main():
+    # Preprocess the data
     train_data = preprocessor.preprocess_train_data()
     test_data_dict = preprocessor.preprocess_test_data()
-        
+    gs_data_dict = preprocessor.preprocess_gs_data()
+
+    # Prepare the tagger
+    tagger = HiddenMarkovModelTagger.train(train_data)
+
+    # Tag test data
+    test_data_tagged = {}
+
+    for key, values in zip(test_data_dict.keys(), test_data_dict.values()):
+        test_data_tagged[key] = []
+
+        for sentence in values:
+            test_data_tagged[key].append(tagger.tag(sentence))
+    
 if __name__ == "__main__":
     main()
--- preprocessing/preprocessor.py
+++ preprocessing/preprocessor.py
@@ -3,27 +3,25 @@ import re
 from nltk import ConditionalFreqDist
 from bs4 import BeautifulSoup
 
-# HELPER FUNCTIONS
+# CONSTANTS
 
-def tokenize(filename):
+taglist = ['PRON', 'SCONJ', 'ADV', 'DET', 'INTJ', 'X', 'PART', 'ADP',
+                'SYM', 'NUM', 'CCONJ', 'VERB', 'PROPN', 'ADJ', 'NOUN']  
+
+# TRAINING 
+
+def preprocess_train_data():
     '''
-    Takes in a .txt filename path
-    Returns a list of tokenized sentences (list of lists)
+    Returns a ConditionalFreqDist with all instances of previously annotated data
+    Untagged words present in the XML file (mostly punctuation words) are ommitted
     '''
-    sentences = []
 
-    with open(filename, 'r') as file:
-        sentences = file.readlines()
+    current_dir = os.getcwd()
+    data_file_path = os.path.join(current_dir, 'data/train/annotated-words-nzi.xml')
 
-    sentences = [remove_punct(sent) for sent in sentences]  # Remove punctuation
-    sentences = [sent.split() for sent in sentences]        # Tokenize
+    return parse_training_xml(data_file_path)
 
-    return sentences
-
-def remove_punct(sentence):
-    return re.sub(r'[^\w\s]', '', sentence)
-
-def read_xml_data(filename):
+def parse_training_xml(filename):
     '''
     Takes in an .xml filename path
     Creates and returns a list of annotated sentences in the form [(word, tag), ...]
@@ -52,21 +50,8 @@ def read_xml_data(filename):
             tagged_sentences.append(sent)
 
         return tagged_sentences
-                
-
-# CONTENT FUNCTIONS 
-
-def preprocess_train_data():
-    '''
-    Returns a ConditionalFreqDist with all instances of previously annotated data
-    Untagged words present in the XML file (mostly punctuation words) are ommitted
-    '''
-
-    current_dir = os.getcwd()
-    data_file_path = os.path.join(current_dir, 'data/train/annotated-words-nzi.xml')
-
-    return read_xml_data(data_file_path)
-
+    
+# TEST
 
 def preprocess_test_data():
     '''
@@ -88,3 +73,86 @@ def preprocess_test_data():
         file_data[file] = (tokenize(path))
 
     return file_data
+
+def tokenize(filename):
+    '''
+    Takes in a .txt filename path
+    Returns a list of tokenized sentences (list of lists)
+    '''
+    sentences = []
+
+    with open(filename, 'r') as file:
+        sentences = file.readlines()
+
+    sentences = [remove_punct(sent) for sent in sentences]  # Remove punctuation
+    sentences = [sent.split() for sent in sentences]        # Tokenize
+
+    return sentences
+
+def remove_punct(sentence):
+    return re.sub(r'[^\w\s]', '', sentence)
+
+# GOLDEN STANDARDS
+
+def preprocess_gs_data():
+    '''
+    Returns a dictionary of preprocessed sentences
+    The {key : value} pairs are as follows -- {filename : list of sentences}
+    '''
+
+    current_dir = os.getcwd()
+    data_file_path = os.path.join(current_dir, 'data/gs/gs-nzi.xml')
+
+    return parse_gs_xml(data_file_path)
+
+def parse_gs_xml(filename):
+    '''
+    Takes in an .xml filename path
+    Creates and returns a list of annotated sentences in the form [(word, tag), ...]
+    '''
+    
+    with open(filename, 'r') as file:
+        data = file.read()
+
+        bs_data = BeautifulSoup(data, 'xml')
+        tagged_sentences = []
+
+        entries = bs_data.find_all('w:rStyle', {'w:val': 'Interlin Base nzi'})
+        raw_tags = []
+        
+        # Code snippet adapted from ChatGPT
+        for tag in entries:
+            base_text_tag = tag.find_next('m:t')
+            pos_tag = tag.find_next('w:rStyle', {'w:val': 'Interlin Word POS'})
+            pos_text_tag = pos_tag.find_next('m:t') if pos_tag is not None else None
+
+            # Removing header
+            if base_text_tag.text.strip() == "PASSAGE":
+                continue
+
+            # Removing whitespace at the end
+            raw_tags.append((base_text_tag.text.strip(), pos_text_tag.text.strip() if pos_text_tag is not None else None))
+
+        gs_tag_dict = {"passage1.txt": [], "passage2.txt": []}
+
+        # Format tuples into sentences
+        sent = []
+        for tup in raw_tags[1:]:
+            # Guard clause to check for passage 2
+            if re.search('2', tup[0]):
+                gs_tag_dict['passage1.txt'] = tagged_sentences
+                tagged_sentences = []
+
+            if not re.search(r'[^\w\s]', tup[0]):
+                    sent.append(tup)
+
+            elif tup[0].endswith('.'):
+                tagged_sentences.append(sent)
+                sent = []
+                continue
+            else:
+                continue
+
+        gs_tag_dict['passage2.txt'] = tagged_sentences
+
+        return gs_tag_dict
